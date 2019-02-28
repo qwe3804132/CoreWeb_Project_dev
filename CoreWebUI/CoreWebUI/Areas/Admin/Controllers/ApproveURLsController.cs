8c47d75a098baaaeaf725a775add50d244fe4dee
Peiran Liu <liupeiran9324@outlook.com>
2019-02-27 19:00:56 -0500

    somehow the approved and rejected now working

--- CoreWebUI/CoreWebUI/Areas/Admin/Controllers/ApproveURLsController.cs
+++ CoreWebUI/CoreWebUI/Areas/Admin/Controllers/ApproveURLsController.cs
@@ -6,12 +6,57 @@ using System.Web.Mvc;
 
 namespace CoreWebUI.Areas.Admin.Controllers
 {
-    public class ApproveURLsController : Controller
+    public class ApproveURLsController : BaseAdminController
     {
         // GET: Admin/ApproveURLs
-        public ActionResult Index()
+        public ActionResult Index(string Status)
         {
-            return View();
+            ViewBag.Status = (Status == null ? "P" : Status);
+            if (Status == null)
+            {
+                var urls = objBs.urlBs.GetAll().Where(x => x.IsApproved == "P").ToList();
+                return View(urls);
+            }
+            else
+            {
+                var urls = objBs.urlBs.GetAll().Where(x => x.IsApproved == Status).ToList();
+                return View(urls);
+
+            }
+        }
+
+        public ActionResult Approve(int id)
+        {
+            try
+            {
+                var myUrl = objBs.urlBs.GetByID(id);
+                myUrl.IsApproved = "A";
+                objBs.urlBs.Update(myUrl);
+                TempData["Msg"] = "Approved Successfully";
+                return RedirectToAction("Index");
+            }
+            catch(Exception e1)
+            {
+                TempData["Msg"] = "Approval Filed:" + e1.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Reject(int id)
+        {
+            try
+            {
+                var myUrl = objBs.urlBs.GetByID(id);
+                myUrl.IsApproved = "R";
+                objBs.urlBs.Update(myUrl);
+                TempData["Msg"] = "Approved Successfully";
+                return RedirectToAction("Index");
+            }
+            catch (Exception e1)
+            {
+                TempData["Msg"] = "Rejection Filed:" + e1.Message;
+                return RedirectToAction("Index");
+            }
         }
     }
 }
